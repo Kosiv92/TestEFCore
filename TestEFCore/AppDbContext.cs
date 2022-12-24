081631aea3b9371084f7a8819cfcffd08dd7a782
kosiv92 <kosiv92@gmail.com>
2022-12-24 20:41:16 +0800

    Try with anonymous types

--- TestEFCore/AppDbContext.cs
+++ TestEFCore/AppDbContext.cs
@@ -14,10 +14,29 @@ namespace TestEFCore
         public AppDbContext() => Database.EnsureCreated();
 
         protected override void OnConfiguring(DbContextOptionsBuilder ob) => ob.UseSqlite("Data Source=ApplicationDb");
-        
+
         protected override void OnModelCreating(ModelBuilder mb)
         {
-            mb.Entity<Role>().HasData(DataFactory.Roles);
+            var roles = DataFactory.Roles;
+            var role1 = roles.FirstOrDefault(r => r.Id == Guid.Parse("53729686-a368-4eeb-8bfa-cc69b6050d02"));
+            var role2 = roles.FirstOrDefault(r => r.Id == Guid.Parse("b0ae7aac-5493-45cd-ad16-87426a5e7665"));
+
+            mb.Entity<Role>()
+                .OwnsMany(r => r.Employees)
+                .HasData(
+                new
+                {
+                    Id = role1.Id,
+                    Name = role1.Name,
+                    Employees = role1.Employees
+                },
+                new
+                {
+                    Id = role2.Id,
+                    Name = role2.Name,
+                    Employees = role2.Employees
+                }
+                );
         }
 
     }
--- TestEFCore/DataFactory.cs
+++ TestEFCore/DataFactory.cs
@@ -35,14 +35,14 @@ namespace TestEFCore
             new Employee()
             {
                 Id = Guid.Parse("451533d5-d8d5-4a11-9c7b-eb9f14e1a32f"),
-                Name = "Иван"
-                //Role = Roles.FirstOrDefault(r => r.Id == Guid.Parse("53729686-a368-4eeb-8bfa-cc69b6050d02"))
+                Name = "Иван",
+                Role = Roles.FirstOrDefault(r => r.Id == Guid.Parse("53729686-a368-4eeb-8bfa-cc69b6050d02"))
             },
             new Employee()
             {
                 Id = Guid.Parse("f766e2bf-340a-46ea-bff3-f1700b435895"),
-                Name = "Петр"
-                //Role = Roles.FirstOrDefault(r => r.Id == Guid.Parse("b0ae7aac-5493-45cd-ad16-87426a5e7665"))
+                Name = "Петр",
+                Role = Roles.FirstOrDefault(r => r.Id == Guid.Parse("b0ae7aac-5493-45cd-ad16-87426a5e7665"))
             }
         };
 
